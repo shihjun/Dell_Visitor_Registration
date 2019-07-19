52ef8859d36d53a9cf0b063b05907a97d2d53c75
Ong <Ong_Chun_Tat_Dastan@dell.com>
2019-07-19 18:23:40 +0800

    angular frontend

--- angular/visitorregistration/src/app/app.module.ts
+++ angular/visitorregistration/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SubmitRequestComponent } from './submit-request/submit-request.component';
@@ -8,6 +8,8 @@ import { RequestListComponent } from './request-list/request-list.component';
 import { RequestDetailsComponent } from './request-details/request-details.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatDatepickerModule, MatNativeDateModule, MatButtonModule, MatFormFieldModule, MatInputModule, MatRippleModule } from '@angular/material';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -25,7 +27,10 @@ import { MatDatepickerModule, MatNativeDateModule, MatButtonModule, MatFormField
     MatButtonModule,
     MatFormFieldModule,
     MatInputModule,
-    MatRippleModule
+    MatRippleModule,
+    ReactiveFormsModule,
+    MatAutocompleteModule,
+    HttpClientModule
   ],
   providers: [
   ],
--- angular/visitorregistration/src/app/submit-request/submit-request.component.ts
+++ angular/visitorregistration/src/app/submit-request/submit-request.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { RequestService } from '../request.service';
 
 @Component({
   selector: 'app-submit-request',
@@ -7,9 +9,61 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SubmitRequestComponent implements OnInit {
 
-  constructor() { }
+  requestForm = new FormGroup({
+    visitorName: new FormControl(""),
+    phoneNumber: new FormControl(""),
+    icNumber: new FormControl(""),
+    carplateNumber: new FormControl(""),
+    visitOnDate: new FormControl(""),
+    visitOnTime: new FormControl(""),
+    visitToDate: new FormControl(""),
+    visitToTime: new FormControl(""),
+    visitReason: new FormControl(""),
+    primaryCN: new FormControl(""),
+    alternativeCN: new FormControl(""),
+    primaryBadge: new FormControl(""),
+    alternativeBadge: new FormControl(""),
+    primaryPhone: new FormControl(""),
+    alternativePhone: new FormControl("")
+  });
+
+  constructor(private requestService: RequestService) { }
+    users: any;
 
   ngOnInit() {
+    
   }
 
+  onSubmit() {
+    const { visitOnDate, visitOnTime, visitToDate, visitToTime } = this.requestForm.value
+
+    // const visitFrom = visitOnDate.toDateString() + " " + visitOnTime
+    // const visitTo = visitToDate.toDateString() + " " + visitToTime
+
+    this.requestService.getUser().subscribe(response => {
+      this.users = response
+      console.log(this.users)
+    })
+
+    const data = {
+      name: this.requestForm.value.visitorName,
+      ic: this.requestForm.value.icNumber,
+      phone: this.requestForm.value.phoneNumber,
+      carPlate: this.requestForm.value.carplateNumber,
+      visitFrom: "7/19/2019 04:30:00.0",
+      visitTo: "7/19/2019 04:30:00.0",
+      purpose: this.requestForm.value.visitReason,
+      department: "IT",
+      status: "New",
+      primaryContactPhone: this.requestForm.value.primaryPhone,
+      alternativeContactPhone: this.requestForm.value.alternativePhone,
+      createdAt: "7/19/2019 04:30:00.0",
+      updatedAt: "7/19/2019 04:30:00.0"
+    }
+
+    if(!this.requestForm.invalid) {
+      console.log(data)
+      this.requestService.createRequest(data)
+    }
+  }
 }
