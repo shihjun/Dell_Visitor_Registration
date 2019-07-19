c2f5daa5f1e5e92991406a46d39e8340dfb31b9f
Ong <Ong_Chun_Tat_Dastan@dell.com>
2019-07-19 11:29:53 +0800

    Datetime Picker complete

--- angular/visitorregistration/src/app/app.module.ts
+++ angular/visitorregistration/src/app/app.module.ts
@@ -3,16 +3,32 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SubmitRequestComponent } from './submit-request/submit-request.component';
+import { RequestListComponent } from './request-list/request-list.component';
+import { RequestDetailsComponent } from './request-details/request-details.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatDatepickerModule, MatNativeDateModule, MatButtonModule, MatFormFieldModule, MatInputModule, MatRippleModule } from '@angular/material';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SubmitRequestComponent,
+    RequestListComponent,
+    RequestDetailsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatRippleModule
   ],
-  providers: [],
+  providers: [
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ angular/visitorregistration/src/app/submit-request/submit-request.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-submit-request',
+  templateUrl: './submit-request.component.html',
+  styleUrls: ['./submit-request.component.css']
+})
+export class SubmitRequestComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
