67bbeb52286a1c3fc0f4e94dedd4f42ba01076d1
shih_jun_teh <shih_jun_teh@dell.com>
2019-08-07 12:07:14 +0800

    spring

--- spring/visitorregistration/src/main/java/com/example/visitorregistration/repositories/RequestRepository.java
+++ spring/visitorregistration/src/main/java/com/example/visitorregistration/repositories/RequestRepository.java
@@ -5,12 +5,13 @@ import java.util.List;
 import com.example.visitorregistration.entities.Request;
 import com.example.visitorregistration.entities.User;
 
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 /**
  * RequestRepository
  */
 public interface RequestRepository extends JpaRepository<Request, Long> {
-  List<Request> findAllByCreatedBy(User createdBy);
+  List<Request> findAllByCreatedBy(User createdBy, Sort Sort);
 
 }
